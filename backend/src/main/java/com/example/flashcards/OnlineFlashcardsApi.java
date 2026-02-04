c11320519b5f4bee4448dfbb1ff5a4476b4997d9
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2026-02-04 20:50:55 +0200

    feat: update configuration files for database connection, JWT, and logging; rename main class and add Maven wrapper

--- backend/src/main/java/com/example/flashcards/OnlineFlashcardsApi.java
+++ backend/src/main/java/com/example/flashcards/OnlineFlashcardsApi.java
@@ -6,11 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
-
 @SpringBootApplication
-public class Main {
+public class OnlineFlashcardsApi {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(OnlineFlashcardsApi.class, args);
     }
 }
--- backend/src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
--- Example data for dev
-INSERT INTO users (id, username) VALUES (1, 'aleksput');
