e0f77fa6bfdec2fa7738d5db2b2c2781e1726e42
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2026-02-05 22:20:12 +0200

    Add cores for Flashcard, Quiz and Subject entities

--- backend/src/main/java/com/example/flashcards/entity/flashcard/Flashcard.java
+++ backend/src/main/java/com/example/flashcards/entity/flashcard/Flashcard.java
@@ -1,5 +1,6 @@
 package com.example.flashcards.entity.flashcard;
 
+import com.example.flashcards.entity.quiz.Quiz;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
@@ -20,4 +21,57 @@ public class Flashcard {
 
     @Column(nullable = false, length = 255)
     private String answer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_id", nullable = false)
+    private Quiz quiz;
+
+    protected Flashcard() {
+    }
+
+    public Flashcard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public long getFlashcardId() {
+        return this.flashcardId;
+    }
+
+    public String getQuestion() {
+        return this.question;
+    }
+
+    public String getAnswer() {
+        return this.answer;
+    }
+
+    public Quiz getQuiz() {
+        return this.quiz;
+    }
+
+    public void setFlashcardId(long flashcardId) {
+        this.flashcardId = flashcardId;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
+    @Override
+    public String toString() {
+        return "Flashcard{" +
+                "flashcardId=" + flashcardId +
+                ", question='" + question + '\'' +
+                ", answer='" + answer + '\'' +
+                '}';
+    }
 }
--- backend/src/main/java/com/example/flashcards/entity/quiz/Quiz.java
+++ backend/src/main/java/com/example/flashcards/entity/quiz/Quiz.java
@@ -1,4 +1,109 @@
 package com.example.flashcards.entity.quiz;
 
+import com.example.flashcards.entity.flashcard.Flashcard;
+import com.example.flashcards.entity.subject.Subject;
+import com.example.flashcards.entity.user.User;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "subjects")
 public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long quizId;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(nullable = true, length = 255)
+    private String description;
+
+    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Flashcard> flashcards = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creator_id", nullable = false)
+    private User creator;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subject_id", nullable = false)
+    private Subject subject;
+
+    protected Quiz() {
+    }
+
+    public Quiz(String title, String description, User creator, Subject subject) {
+        this.title = title;
+        this.description = description;
+        this.creator = creator;
+        this.subject = subject;
+    }
+
+    public long getQuizId() {
+        return this.quizId;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public List<Flashcard> getFlashcards() {
+        return this.flashcards;
+    }
+
+    public User getCreator() {
+        return this.creator;
+    }
+
+    public Subject getSubject() {
+        return this.subject;
+    }
+
+    public void setQuizId(long quizId) {
+        this.quizId = quizId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public void addFlashcard(Flashcard flashcard) {
+        flashcards.add(flashcard);
+        flashcard.setQuiz(this);
+    }
+
+    public void removeFlashcard(Flashcard flashcard) {
+        flashcards.remove(flashcard);
+        flashcard.setQuiz(null);
+    }
+
+    @Override
+    public String toString() {
+        return "Quiz{" +
+                "quizId=" + quizId +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", creator=" + creator.getUsername() +
+                ", subject=" + subject.getName() +
+                '}';
+    }
 }
