f275ec110f3988b514b87834f0b40931ca5cb6f6
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2026-02-08 16:51:52 +0200

    feat: update application configuration to create database if not exists and remove Flyway dependencies

--- backend/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
--- Example data for dev
-DELETE FROM flashcards.users;
-INSERT INTO flashcards.users (user_id, username, email, password, role) VALUES (1, 'aleksput', 'aleksi@aleksi.fi', '$2a$12$aaBCKsjDrT2IWffmLoLWQO/BzWz26ulrf0cJjDVqa.RPbSoyaeKm2', 'STUDENT');
