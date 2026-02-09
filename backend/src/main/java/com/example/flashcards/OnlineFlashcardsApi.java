3691c89d2416a4bd6e133801f95b441637304288
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2026-02-09 15:41:48 +0200

    Remove unused imports from api entrypoint

--- backend/src/main/java/com/example/flashcards/OnlineFlashcardsApi.java
+++ backend/src/main/java/com/example/flashcards/OnlineFlashcardsApi.java
@@ -1,11 +1,7 @@
 package com.example.flashcards;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class OnlineFlashcardsApi {
