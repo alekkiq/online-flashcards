9dcf927a0488677533ab05301359a2680ed7d085
Blendi Grajqevci <blendi.gr@hotmail.fi>
2026-02-24 20:12:12 +0200

    feat: Add API functions for quiz management, including CRUD operations, search, and fetching user's quizzes.

--- frontend/src/api/quizApi.js
+++ frontend/src/api/quizApi.js
@@ -1,35 +1,50 @@
 import { fetchData } from "/src/lib/fetchData";
 
+const token = localStorage.getItem("token");
+
 export async function getQuiz(id) {
-  return fetchData(`quiz/${id}`);
+  return fetchData(`quizzes/${id}`);
 }
 
 export async function createQuiz(quiz) {
-  return fetchData("quiz", {
+  return fetchData("quizzes", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(quiz),
   });
 }
 
+export async function searchQuizzes(query) {
+  return fetchData(`quizzes/search?title=${query}`);
+}
+
 export async function updateQuiz(id, quiz) {
-  return fetchData(`quiz/${id}`, {
+  return fetchData(`quizzes/${id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(quiz),
   });
 }
 
 export async function deleteQuiz(id) {
-  return fetchData(`quiz/${id}`, {
+  return fetchData(`quizzes/${id}`, {
     method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   });
 }
 
 export async function getMyQuizzes() {
-  return fetchData("quiz/my");
+  return fetchData("quizzes/me", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 }
