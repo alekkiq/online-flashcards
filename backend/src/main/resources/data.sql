8c8042216ebf5c739bb6d20209d9a981a49e290e
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2026-03-01 01:38:55 +0200

    feat: enable SQL initialization on application startup and add subject seeding script

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- seed subjects
+INSERT IGNORE INTO subjects (name) VALUES ('Mathematics');
+INSERT IGNORE INTO subjects (name) VALUES ('Science');
+INSERT IGNORE INTO subjects (name) VALUES ('History');
+INSERT IGNORE INTO subjects (name) VALUES ('Literature');
+INSERT IGNORE INTO subjects (name) VALUES ('Programming');
+INSERT IGNORE INTO subjects (name) VALUES ('Others');
