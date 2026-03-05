b9601293549524c978daec34c9d2e2e545168acb
Blendi Grajqevci <blendi.gr@hotmail.fi>
2026-03-05 12:44:28 +0200

    fix

--- backend/src/main/java/com/example/flashcards/OnlineFlashcardsApi.java
+++ backend/src/main/java/com/example/flashcards/OnlineFlashcardsApi.java
@@ -1,11 +1,23 @@
 package com.example.flashcards;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineFlashcardsApi {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+            .ignoreIfMissing()
+            .load();
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
+        Dotenv parentDotenv = Dotenv.configure()
+            .directory("../")
+            .ignoreIfMissing()
+            .load();
+        parentDotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
         SpringApplication.run(OnlineFlashcardsApi.class, args);
     }
 }
