9c080bc07f497ccd94da33db600b374f61aa6aa8
Aleksi Putkonen <aleksi.putkonen1@gmail.com>
2026-03-24 11:10:50 +0200

    Add languages to config

---
+++ frontend/src/config/languages.js
@@ -0,0 +1,26 @@
+export const LANGUAGES = {
+    fi: {
+        locale: 'fi_FI',
+        lng: 'fi',
+        label: 'Suomi',
+        isRtl: false,
+    },
+    en: {
+        locale: 'en_UK',
+        lng: 'en',
+        label: 'English',
+        isRtl: false,
+    },
+    fa: {
+        locale: 'fa_IR',
+        lng: 'fa',
+        label: 'Farsi',
+        isRtl: true,
+    },
+    zh: {
+        locale: 'zh_CN',
+        lng: 'zh',
+        label: 'Mandarin',
+        isRtl: false,
+    },
+};
